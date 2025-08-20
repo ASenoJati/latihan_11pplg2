2c20969d5e4e9d564b40a969e113d47419e1cba7
ASenoJati <senojati16@gmail.com>
2025-08-20 10:39:16 +0700

    Feat [SEN] Profile Page

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Profile")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Center(
+              child: CircleAvatar(
+                radius: 40,
+                backgroundImage: AssetImage("images/05542.jpg"),
+              ),
+            ),
+            SizedBox(height: 20),
+
+            Padding(
+              padding: EdgeInsets.only(bottom: 12.0),
+              child: Text("A Seno Jati"),
+            ),
+
+            Padding(
+              padding: EdgeInsets.only(bottom: 12.0),
+              child: Text("senojati16@gmail.com"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/routes/pages.dart
+++ lib/routes/pages.dart
@@ -3,6 +3,7 @@ import 'package:latihan_11pplg2/pages/calculator_page.dart';
 import 'package:latihan_11pplg2/pages/edit_football_page.dart';
 import 'package:latihan_11pplg2/pages/football_player.dart';
 import 'package:latihan_11pplg2/pages/main_page.dart';
+import 'package:latihan_11pplg2/pages/profile_page.dart';
 import 'package:latihan_11pplg2/routes/routes.dart';
 
 class AppPages {
@@ -10,6 +11,7 @@ class AppPages {
     GetPage(name: AppRoutes.mainPage, page: () => MainPage()),
     GetPage(name: AppRoutes.calculatorPage, page: () => CalculatorPage()),
     GetPage(name: AppRoutes.football, page: () => FootballPlayer()),
+    GetPage(name: AppRoutes.profilePage, page: () => ProfilePage()),
     GetPage(name: AppRoutes.editFootball, page: () => EditFootballPage()),
   ];
 }
